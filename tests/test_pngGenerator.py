61374a60c7ef4205fc667b278485149ea95184f0
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-04-01 16:26:57 +0200

    ajout premier test

--- tests/test_pngGenerator.py
+++ tests/test_pngGenerator.py
@@ -1,7 +1,26 @@
 import unittest
-from PngGenerator import PngBuilder
+from PngGenerator import PngBuilder, ColorType
+import struct
 
-class PngBuilderTestCase(unittest.TestCase):
-    
-    def test_mandatoryPlte(self):
-        pass
+class PngBuilderRGBTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.imgData = []
+
+        row =0
+        while row < 32:
+            col = 0
+            rowData = []
+            while col < 32:
+                rowData.append([255,255,255])
+                col+=1
+            self.imgData.append(rowData)	
+            row+=1
+
+        self.photoBuilder = PngBuilder(32,32,ColorType.RGB)
+        self.photoBuilder.addIDATChunk(self.imgData)
+        
+    def test_pngMagicNumberCompliant(self):
+        
+        data=self.photoBuilder.getFileByteContent()
+        self.assertEqual(data[0:8], b'\x89PNG\r\n\x1a\n')
